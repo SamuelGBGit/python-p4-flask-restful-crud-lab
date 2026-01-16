144e8f438ee2333370b2eac7479a79680ea90947
Taida <samuelgbusinessacc@gmail.com>
2026-01-16 14:34:22 +0300

    a

--- server/testing/conftest.py
+++ server/testing/conftest.py
@@ -1,5 +1,40 @@
 #!/usr/bin/env python3
 
+import pytest
+from app import app
+from models import db, Plant
+
+@pytest.fixture(autouse=True)
+def setup_database():
+    with app.app_context():
+        db.create_all()
+        
+        Plant.query.delete()
+        
+        aloe = Plant(
+            id=1,
+            name="Aloe",
+            image="./images/aloe.jpg",
+            price=11.50,
+            is_in_stock=True,
+        )
+        
+        zz_plant = Plant(
+            id=2,
+            name="ZZ Plant",
+            image="./images/zz-plant.jpg",
+            price=25.98,
+            is_in_stock=False,
+        )
+        
+        db.session.add_all([aloe, zz_plant])
+        db.session.commit()
+        
+        yield
+        
+        db.session.remove()
+        db.drop_all()
+
 def pytest_itemcollected(item):
     par = item.parent.obj
     node = item.obj
